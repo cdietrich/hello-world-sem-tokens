d09def63521bb8ea091c4ff68c8ec6ce67c47efa
Christian Dietrich <christian.dietrich.opensource@gmail.com>
2023-11-10 07:01:24 +0100

    initial commit

---
+++ src/language/main-browser.ts
@@ -0,0 +1,17 @@
+import { EmptyFileSystem, startLanguageServer } from 'langium';
+import { BrowserMessageReader, BrowserMessageWriter, createConnection } from 'vscode-languageserver/browser.js';
+import { createHelloWorldServices } from './hello-world-module.js';
+
+declare const self: DedicatedWorkerGlobalScope;
+
+const messageReader = new BrowserMessageReader(self);
+const messageWriter = new BrowserMessageWriter(self);
+
+const connection = createConnection(messageReader, messageWriter);
+
+const { shared } = createHelloWorldServices({ connection, ...EmptyFileSystem });
+// connection.workspace.onDidDeleteFiles((params) => {
+//     connection.workspace.onDidChange([], params.files.map((f) => f.uri))
+// })
+
+startLanguageServer(shared);
---
+++ static/setupClassic.js
@@ -0,0 +1,33 @@
+import { addMonacoStyles, defineUserServices, MonacoEditorLanguageClientWrapper } from './bundle/index.js';
+import monarchSyntax from "../syntaxes/hello-world.monarch.js";
+import { configureWorker } from './setup.js';
+
+addMonacoStyles('monaco-editor-styles');
+
+export const setupConfigClassic = () => {
+    return {
+        wrapperConfig: {
+            serviceConfig: defineUserServices(),
+            editorAppConfig: {
+                $type: 'classic',
+                languageId: 'hello-world',
+                code: `// Hello World is running in the web!
+person A Hello A! Hello B!`,
+                useDiffEditor: false,
+                languageExtensionConfig: { id: 'langium' },
+                // languageDef: monarchSyntax,
+                editorOptions: {
+                    'semanticHighlighting.enabled': true,
+                    //theme: 'vs-dark'
+                }
+            }
+        },
+        languageClientConfig: configureWorker()
+    };
+};
+
+export const executeClassic = async (htmlElement) => {
+    const userConfig = setupConfigClassic();
+    const wrapper = new MonacoEditorLanguageClientWrapper();
+    await wrapper.start(userConfig, htmlElement);
+};
